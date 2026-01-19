cbfbc0bdcbdf360a6bc30bbb60a8361eedce768c
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-01-18 21:17:07 -0300

    Criação de um projeto simples para pratica de polimorfismo

---
+++ Poli/TestePolimorfismo/src/Calcula.java
@@ -0,0 +1,11 @@
+public class Calcula {
+
+    public double multiplica(double n1, double n2) {
+        return n1 * n2;
+    }
+
+    public int multiplica(int n1, int n2) {
+        return n1 * n2;
+    }
+
+}
