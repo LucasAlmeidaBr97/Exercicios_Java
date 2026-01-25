d3fc4b652ef9e66b36d830504a218d9c00557269
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-01-25 20:52:46 -0300

    Criação da SolutionTest
    Foi criada uma classe SolutionTeste contendo o metodo ststicTest() que testa se o método solutuin() na classe Kata realmente funciona

---
+++ CompareString/compare/src/test/java/com/compare/SolutionTest.java
@@ -0,0 +1,37 @@
+
+package com.compare;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SolutionTest {
+
+    @Test
+    public void staticTest() {
+        check("samurai", "ai", true);
+        check("sumo", "omo", false);
+        check("ninja", "ja", true);
+        check("sensei", "i", true);
+        check("samurai", "ra", false);
+        check("abc", "abcd", false);
+        check("abc", "abc", true);
+        check("abcabc", "bc", true);
+        check("ails", "fails", false);
+        check("fails", "ails", true);
+        check("this", "fails", false);
+        check("this", "", true);
+        check(":-)", ":-(", false);
+        check("!@#$%^&*() :-)", ":-)", true);
+        check("abc\n", "abc", false);
+    }
+
+    private static void check(String str, String ending, boolean expected) {
+        boolean result = Kata.solution(str, ending);
+        assertEquals(
+                expected,
+                result,
+                "Expected solution(\"" + str + "\", \"" + ending + "\") to return " + expected);
+    }
+
+}
