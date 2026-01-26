a7c87a8148dfde4544ed5185599e6ea5a2fdb52f
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-01-25 22:12:03 -0300

    Exercicio simples java
    Exercicio Alphabet Position associar numeros a letras e encontrar o numero de cara letra de uma string informada

---
+++ AlphabetPosition/alphabet/src/main/java/com/alphabet/position/App.java
@@ -0,0 +1,10 @@
+package com.alphabet.position;
+
+
+public final class App {
+
+    public static void main(String[] args) {
+        Kata kata = new Kata();
+        System.out.println(kata.alphabetPosition("abobora"));
+    }
+}
