fea4886415ab9be77e54bcde0211d948cf8beef2
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-01-28 20:19:46 -0300

    Método melhorado de calcular próximo quadrado perfeito

--- NextSquare/NextSquare/src/App.java
+++ NextSquare/NextSquare/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         NumberFun numberFun = new NumberFun();
-        System.out.println(numberFun.findNextSquare(2));
+        System.out.println(numberFun.findNextSquare(1));
+        System.out.println(numberFun.findCorrectNextSquare(16));
     }
 }
--- NextSquare/NextSquare/src/NumberFun.java
+++ NextSquare/NextSquare/src/NumberFun.java
@@ -1,14 +1,28 @@
 public class NumberFun {
 
-        public static long findNextSquare(long sq){
-            long nextNum = (long)Math.sqrt(sq) + 1;
-            long nextSqr = (long)Math.pow(nextNum,2);
-            double sqrRoot = Math.sqrt(sq);
-            if (sqrRoot % 1 != 0 || sq < 0) {
-                return -1;    
-            }
-            return nextSqr;
+    public static long findNextSquare(long sq) {
+        long nextNum = (long) Math.sqrt(sq) + 1;
+        long nextSqr = (long) Math.pow(nextNum, 2);
+        double sqrRoot = Math.sqrt(sq);
+        if (sqrRoot % 1 != 0 || sq < 0) {
+            return -1;
+        }
+        return nextSqr;
+    }
+
+    public static long findCorrectNextSquare(long sq) {
+        if (sq < 0) {
+            return -1;
         }
 
+        long root = (long) Math.sqrt(sq);
+
+        if (root * root != sq) {
+            return -1;
+        }
+
+        long next = root + 1;
+        return next * next;
+    }
 
 }
