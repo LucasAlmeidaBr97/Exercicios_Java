ed28edec4a9d07da567896df1267237435fdec9d
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-03-05 20:24:21 -0300

    JButton Lambda

--- FuncoesLambda/Aula1/Lambda/src/App.java
+++ FuncoesLambda/Aula1/Lambda/src/App.java
@@ -1,23 +1,28 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
         // Java 8: FUNÇÕES LAMBDA
 
-        new Thread(new Runnable() {
+        JButton jButton = new JButton();
+        jButton.addActionListener(new ActionListener() {
+
             @Override
-            public void run() {
-                System.out.println("Olá mundo!");
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Olá mundo");
             }
-        }).run();
+        });
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                System.out.println("Olá mundo!");
-            }
-        }).run();
 
-        new Thread(() -> System.out.println("Olá mundo!")).run();
+        JButton jButton2 = new JButton();
+        jButton2.addActionListener(e -> System.out.println("Olá Mundo!"));
 
+
+        //SAM - Single Abstract Method
+        //Qualquer Interface que tenha apenas um método abstrato.
     }
 }
