0ca036f1ab3ab641b63997c02f21873791cf2690
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-03-05 20:06:16 -0300

    Criando uma Thread Java 7-

--- FuncoesLambda/Aula1/Lambda/src/App.java
+++ FuncoesLambda/Aula1/Lambda/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // Java 8: FUNÇÕES LAMBDA
+
+        new Thread(new Runnable () {
+            @Override
+            public void run() {
+                System.out.println("Olá mundo!");
+            }
+        }).run();
+
+        
     }
 }
