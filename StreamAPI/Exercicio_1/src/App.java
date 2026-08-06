a1c3fda490ddb0c66341c1e5f8e1fc14fd4a4a0e
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-08-05 21:30:46 -0300

    Exercício 2 - Converter para maiúsculas
    'List<Sring> nomes (...)'. Retorne uma lista contendo todos os nomes em maiúsculo.

--- StreamAPI/Exercicio_1/src/App.java
+++ StreamAPI/Exercicio_1/src/App.java
@@ -4,17 +4,36 @@ import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        converteMais();
+    }
+
+    // Exercício 1
+    public static void pegaPares() {
         List<Integer> numeros = Arrays.asList(3, 8, 15, 22, 9, 14, 30, 11);
         List<Integer> numerosPares = numeros.stream()
                 .filter(n -> n % 2 == 0)
                 .collect(Collectors.toList());
-                
-                
 
-                numerosPares.add(6);
-                numerosPares.remove(2);
-                
-                System.out.println(numerosPares);
+        numerosPares.add(6);
+        numerosPares.remove(2);
+
+        System.out.println(numerosPares);
     }
-    
+
+    // Exercício 2
+    public static void converteMais() {
+        List<String> nomes = Arrays.asList(
+                "Lucas",
+                "Ana",
+                "Carlos",
+                "Maria"
+        );
+
+        List<String> nomesMaísculos = nomes.stream()
+            .map(n -> n.toUpperCase())
+            .collect(Collectors.toList());
+
+        System.out.println( nomesMaísculos);
+    }
+
 }
