021b9893b1fada762b39cff161705f6b1c3ce52e
Lucas Tadeu Almeida Martins de Oliveira <143283670+LucasAlmeidaBr97@users.noreply.github.com>
2026-08-12 18:39:43 -0300

    Exercicio 3 e 4
    3. Dobrar os valores; 4.Contar elementos.

--- StreamAPI/Exercicio_1/src/Exercicio2.java
+++ StreamAPI/Exercicio_1/src/Exercicio2.java
@@ -15,5 +15,31 @@ public class Exercicio2 {
             .collect(Collectors.toList());
 
         capitalNames.forEach(name -> {System.out.println(name);});
+
+        List<Integer> values = doubValue(Arrays.asList(2,4,6,8));
+        System.out.println(capitalNames);
+
+        System.out.println(countElements(Arrays.asList("João",
+            "Pedro",
+            "Lucas",
+            "Maria",
+            "José"
+        )));
     }
+
+
+    //Exercicio 3
+    public static List<Integer> doubValue (List<Integer> numbers) {
+        return numbers.stream()
+            .map(n -> n * 2)
+            .collect(Collectors.toList());
+        
+    }
+
+    //Exercicio 4
+    public static Integer countElements (List<String> names){
+        return (int) names.stream()
+        .count();
+    }
+
 }
